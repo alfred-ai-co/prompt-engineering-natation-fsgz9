01f65e74d369f52589e88d30e0f2ff10cb741e96
Vic <natation@users.noreply.github.com>
2024-12-30 02:47:22 -0800

    Remove {format_instructions} in zero_shot.py

--- prompts/zero_shot.py
+++ prompts/zero_shot.py
@@ -1,5 +1,5 @@
 ZERO_SHOT_PROMPT = """
-Given the following topic {input}, generate a quiz with a random number of questions between 1 to 5.
-
-{format_instructions}
+Generate a quiz on {input} with one multiple choice question (4 choices).
+The output should include a 'questions' key with a list of question objects.
+Each question object should have 'question', 'choices', and 'answer' keys. The choices key should be in "key": "A", "value": "value" format
 """
