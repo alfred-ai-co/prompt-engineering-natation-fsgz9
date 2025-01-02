19227c3ed47145a1502bf37559592afeabd39541
Vic <natation@users.noreply.github.com>
2025-01-02 02:16:39 -0800

    Few shot prompt

--- prompts/few_shot.py
+++ prompts/few_shot.py
@@ -1,3 +1,6 @@
 FEW_SHOT_PROMPT = """
+Generate a quiz on {input} with one multiple choice question (5 choices).
+The output should be in JSON.
 
+Example output for a quiz is: questions=[Question(question='What is the primary leavening agent in most traditional sponge cakes?', choices=[Choice(key='A', value='Baking soda'), Choice(key='B', value='Baking powder'), Choice(key='C', value='Yeast'), Choice(key='D', value='Air beaten into the eggs')], answer='D')])
 """
